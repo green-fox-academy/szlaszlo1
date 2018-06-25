f1b41d70936d5b89badcc4a6cd13aac84ab7aa99
László Szentesi <szentesilaszlo1@gmail.com>
2018-06-25 12:38:08 +0200

    new feat(ASPD01) new api,greeting routes

---
+++ Week 7/DAY1/ASPD01/ASPD01/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPD01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPD01.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting(Greeting greeting)
+        {
+            greeting.Id = 1;
+            greeting.Content = "Hello World!";
+            return new JsonResult(greeting);
+        }
+    }
+}
---
+++ Week 7/DAY1/ASPD01/ASPD01/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPD01.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+    }
+}
