36540b1e189550fa6096e627005bf9cb2dc9ad3a
László Szentesi <szentesilaszlo1@gmail.com>
2018-06-26 13:06:25 +0200

    new feat(RazorPages) Part I-II new class, Endpoint

---
+++ Week 7/DAY2/RazorPagesProject/RazorPagesProject/Controllers/BankAccountHomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorPagesProject.Models;
+
+namespace RazorPagesProject.Controllers
+{
+    //[Route("BankAccountHome")]
+    public class BankAccountHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("Simba")]
+        public IActionResult Simba()
+        {
+            BankAccount ba = new BankAccount("Simba", 2000, "Animal.Lion");
+            return View(ba);
+        }
+
+        public IActionResult Animals()
+        {
+            List<BankAccount> bal = new List<BankAccount> {
+                new BankAccount("Zazu",1000,"Bird"),
+                new BankAccount("Timon",200,"Meerkat"),
+                new BankAccount("Pumba",100,"Warthod"),
+                new BankAccount("Zordon",4000,"Lion"),
+                new BankAccount("Mufasa",10000,"Lion")
+            };
+            return View(bal);
+        }
+    }
+}
---
+++ Week 7/DAY2/RazorPagesProject/RazorPagesProject/Models/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesProject.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string Currency { get; set; }
+
+        public BankAccount(string name,double balance,string animaltype, string currency="Zebra")
+        {
+            Name = name;
+            Balance =balance;
+            AnimalType = AnimalType;
+            Currency = currency;
+        }
+    }
+}
