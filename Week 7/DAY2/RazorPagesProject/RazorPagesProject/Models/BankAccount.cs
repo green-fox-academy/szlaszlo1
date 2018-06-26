f6c828ca130d13716b2d6a49fcc7fff10f711bd5
László Szentesi <szentesilaszlo1@gmail.com>
2018-06-26 13:46:07 +0200

    new feat(RazorPages) Part V IsKing

--- Week 7/DAY2/RazorPagesProject/RazorPagesProject/Models/BankAccount.cs
+++ Week 7/DAY2/RazorPagesProject/RazorPagesProject/Models/BankAccount.cs
@@ -8,11 +8,13 @@ namespace RazorPagesProject.Models
     public class BankAccount
     {
         public static int _id = 0;
+        public static string[] kings = new string[] { "Simba", "Mufasa", "Zordon" };
         public int ID { get; set; }
         public string Name { get; set; }
         public double Balance { get; set; }
         public string AnimalType { get; set; }
         public string Currency { get; set; }
+        public bool King { get; set; }
 
         public BankAccount(string name,double balance,string animaltype, string currency="Zebra")
         {
@@ -21,6 +23,14 @@ namespace RazorPagesProject.Models
             Balance =balance;
             AnimalType = animaltype;
             Currency = currency;
+            if (kings.Contains(name))
+            {
+                King = true;
+            }
+            else
+            {
+                King = false;
+            }
         }
     }
 }
