1f84b7fc33d7847617f3e64e02cedfd1ff399089
László Szentesi <szentesilaszlo1@gmail.com>
2018-06-27 12:55:55 +0200

    new Feat(GreetSomeone) new asp.net core app, new services

---
+++ Week 7/DAY3/GreetSomeone/GreetSomeone/Controllers/GreetingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreetSomeone.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetSomeone.Controllers
+{
+    public class GreetingController : Controller
+    {
+        IGreetName greet;
+        public GreetingController(IGreetName gn)
+        {
+            greet = gn;
+        }
+
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/")]
+        public IActionResult Valami()
+        {
+            return RedirectToAction("greet");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return View(greet.GetName());
+        }
+    }
+}
---
+++ Week 7/DAY3/GreetSomeone/GreetSomeone/Services/GreetName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Services
+{
+    public class GreetName : IGreetName
+    {
+        private string yourName;
+        public string GetName()
+        {
+            return yourName;
+        }
+    }
+}
