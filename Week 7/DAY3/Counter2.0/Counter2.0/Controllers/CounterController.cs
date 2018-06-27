8816f94744fc0de5afadf7f1eb1a25e08ddc99a4
László Szentesi <szentesilaszlo1@gmail.com>
2018-06-27 11:52:36 +0200

    new Feat(Counter2.0) asp.net core app

---
+++ Week 7/DAY3/Counter2.0/Counter2.0/Controllers/CounterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Counter2._0.Controllers
+{
+    public class CounterController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/")]
+        public IActionResult Count()
+        {
+            return RedirectToAction("Index");
+
+        }
+    }
+}
