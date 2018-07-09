dae825fe5ad07b5452cf82e53a13336e1179a614
László Szentesi <szentesilaszlo1@gmail.com>
2018-07-09 14:02:13 +0200

    app(Frontend) greeting

--- Week 9/DAY1/Frontend/Frontend/Controllers/HomeController.cs
+++ Week 9/DAY1/Frontend/Frontend/Controllers/HomeController.cs
@@ -31,5 +31,22 @@ namespace Frontend.Controllers
                 return Json(new { error = "Please provide an input!" });
             }
         }
+
+        [HttpGet("/greeter")]
+        public IActionResult Greeter(string name,string title)
+        {
+            if (name== null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if(title==null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            else
+            {
+                return Json(new { welcome_message = "Oh, hi there " + name + ", my dear " + title + "!" });
+            }
+        }
     }
 }
