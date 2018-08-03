17419a1d5eae535c5f46fffbbbea3c6b62ce87ac
László Szentesi <szentesilaszlo1@gmail.com>
2018-08-03 17:31:23 +0200

    new model(Post)

---
+++ Java/Project/Week0/src/main/java/com/greenfoxacademy/tokenbasedapp/models/Post.java
@@ -0,0 +1,51 @@
+package com.greenfoxacademy.tokenbasedapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String description;
+    private String url;
+    private Integer rating;
+
+    public Post() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
