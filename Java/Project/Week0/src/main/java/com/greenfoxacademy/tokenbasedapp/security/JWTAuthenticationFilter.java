00589785256af3d865f6d5e3f839ef28e2f9fc2b
László Szentesi <szentesilaszlo1@gmail.com>
2018-08-07 10:19:10 +0200

    new feat(JWTAuthenticationFilter) override attemptAuthentication

---
+++ Java/Project/Week0/src/main/java/com/greenfoxacademy/tokenbasedapp/security/JWTAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.greenfoxacademy.tokenbasedapp.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfoxacademy.tokenbasedapp.models.ApplicationUser;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            ApplicationUser applicationUser =new ObjectMapper().readValue(request.getInputStream(), ApplicationUser.class);//parse request to ApplicationUser class
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(applicationUser.getUserName(),applicationUser.getPassword()));
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
